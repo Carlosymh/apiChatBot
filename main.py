92a34d59820d80482ca0f6eef505bd5719a3aace
CarlosY <c.munozh@traxion.global>
2025-08-23 12:16:55 -0600

    init commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.api.endpoints import router
 
-app = FastAPI("Chat Bot API")
+app = FastAPI(title="Chat Bot API", version="1.0.0")
 
 app.include_router(router)
